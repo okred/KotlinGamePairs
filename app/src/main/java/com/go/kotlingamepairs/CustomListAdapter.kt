b2001f01cf75dfd04dbd1d3eb15e1536d1829bf1
Davy Jones <jreally@protonmail.com>
2018-04-20 12:51:32 +0100

    Design Pattern: ViewHolder

--- app/src/main/java/com/go/kotlingamepairs/CustomListAdapter.kt
+++ app/src/main/java/com/go/kotlingamepairs/CustomListAdapter.kt
@@ -7,39 +7,57 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+
 class CustomListAdapter(private val context: Activity, private val name: Array<String>,
                         private val attempts: Array<Int>) : ArrayAdapter<Any>(context, R.layout.listview_row_leaderboard, name) {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
-        val inflater = context.layoutInflater
-        val rowView = inflater.inflate(R.layout.listview_row_leaderboard, parent, false)
 
-        val tvRating = rowView.findViewById<View>(R.id.text_listview_rating) as TextView
-        val tvName = rowView.findViewById<View>(R.id.text_listview_name) as TextView
-        val tvAttempts = rowView.findViewById<View>(R.id.text_listview_attempts) as TextView
+        var convertView = view
 
-        tvName.text = name[position]
-        tvAttempts.text = String.format("%03d", attempts[position])
+        val viewHolder: ViewHolder
+
+        if (convertView == null) {
+
+            val inflater = (context).layoutInflater
+            convertView = inflater.inflate(R.layout.listview_row_leaderboard, parent, false)
+
+            viewHolder = ViewHolder()
+            viewHolder.tvAttempts = convertView!!.findViewById(R.id.text_listview_attempts) as TextView
+            viewHolder.tvName = convertView.findViewById(R.id.text_listview_name) as TextView
+            viewHolder.tvRating = convertView.findViewById(R.id.text_listview_rating) as TextView
+
+            // store the holder with the view.
+            convertView.tag = viewHolder
+
+        } else {
+            // AVOID calling expensive findViewById() repeatedly
+            viewHolder = convertView.tag as ViewHolder
+        }
+
+        //SET attempts, name, rating
+        viewHolder.tvAttempts?.text = String.format("%03d", attempts[position])
+        viewHolder.tvName?.text = name[position]
 
         when(attempts[position]){
-            in 1..50 ->  {tvRating.text = "S"
-                tvRating.setTextColor(Color.WHITE)}
-            in 50..55 -> {tvRating.text = "A"
-                tvRating.setTextColor(Color.GREEN)}
-            in 55..61 -> {tvRating.text = "B"
-                tvRating.setTextColor(Color.YELLOW)}
-            in 61..69 -> {tvRating.text = "C"
-                tvRating.setTextColor(Color.parseColor("#FFA500")) }
-            in 69..79 -> {tvRating.text = "D"
-                tvRating.setTextColor(Color.RED)}
-            in 79..89 -> {tvRating.text = "E"
-                tvRating.setTextColor(Color.parseColor("#A52A2A"))}
-            else  -> {    tvRating.text = "F"
-                tvRating.setTextColor(Color.BLACK)
+            in 1..50 ->  {viewHolder.tvRating?.text = "S"
+                viewHolder.tvRating?.setTextColor(Color.WHITE)}
+            in 50..55 -> {viewHolder.tvRating?.text = "A"
+                viewHolder.tvRating?.setTextColor(Color.GREEN)}
+            in 55..61 -> {viewHolder.tvRating?.text = "B"
+                viewHolder.tvRating?.setTextColor(Color.YELLOW)}
+            in 61..69 -> {viewHolder.tvRating?.text = "C"
+                viewHolder.tvRating?.setTextColor(Color.parseColor("#FFA500")) }
+            in 69..79 -> {viewHolder.tvRating?.text = "D"
+                viewHolder.tvRating?.setTextColor(Color.RED)}
+            in 79..89 -> {viewHolder.tvRating?.text = "E"
+                viewHolder.tvRating?.setTextColor(Color.parseColor("#A52A2A"))}
+            else  -> {    viewHolder.tvRating?.text = "F"
+                viewHolder.tvRating?.setTextColor(Color.BLACK)
             }
         }
 
-        return rowView
+        return convertView
     }
 
 }
---
+++ app/src/main/java/com/go/kotlingamepairs/ViewHolder.kt
@@ -0,0 +1,11 @@
+package com.go.kotlingamepairs
+
+import android.widget.TextView
+
+internal class ViewHolder {
+
+    var tvRating: TextView? = null
+    var tvName: TextView? = null
+    var tvAttempts: TextView? = null
+
+}
